77489b1171d72574a619370e0b2db73110f2f977
Ed Young <ed_young@cable.comcast.com>
2011-01-08 04:24:21 -0700

    added ChimpsSpec.groovy and added .classpath and .project

---
+++ src/test/groovy/ChimpsSpec.groovy
@@ -0,0 +1,41 @@
+import spock.lang.Stepwise
+
+import org.openqa.selenium.Keys
+
+import pages.FrontPage
+import pages.ContactUsPage
+import geb.Browser; 
+
+@Stepwise
+class ChimpsSpec extends BaseSpec {
+	def "at the front page"() {
+		when:
+		to FrontPage
+
+		then:
+		at FrontPage
+	}
+
+	def "drive chimps"() {
+		when:
+		Browser.drive("http://localhost:9090/chimps") {
+			assert title == "Login"
+			println "looking for the login text field"
+			println $("td").size()
+			println $("td")
+			
+			println $("form")
+			println $("table")
+			println $("table", class: "simpleTable")
+			println $("table", class: "simpleTable", id:"left_table")
+			println $("table", class: "simpleTable", id:"left_table")
+			def table = $("table", class: "simpleTable", id:"left_table")
+			println table
+			println $("tr")
+			println $("tr",2).text()
+		}
+		
+		then:
+		assert true
+	}
+}
